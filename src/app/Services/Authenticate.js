0dc0dfbb55a2450bad13eafb8abdb15ea5b085fe
Hieupv <hieupv@codersvn.com>
2018-10-01 11:34:17 +0700

    release ver 2.1

---
+++ src/app/Services/Authenticate.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+import models from '../../../../../../../../models';
+import { UnauthorizedHttpException, Exception } from '@nsilly/exceptions';
+
+export class Authenticate {
+  async login(data) {
+    this.decoded = data;
+    this.isAuthenticated = true;
+  }
+
+  /**
+   * Get current logged in user
+   *
+   * @return object
+   */
+  async getUser() {
+    if (_.isUndefined(models)) {
+      throw new Exception('Method is not implemented');
+    }
+    if (!this.isAuthenticated) {
+      throw new UnauthorizedHttpException('Unauthorized');
+    }
+    if (_.isUndefined(process.domain._req.locals)) {
+      process.domain._req.locals = {};
+    }
+
+    if (_.isUndefined(process.domain._req.locals.user)) {
+      const user = await models.user.findOne({ where: { email: this.decoded.email }, include: [{ model: models.role }] });
+      process.domain._req.locals.user = user;
+    }
+
+    return process.domain._req.locals.user;
+  }
+
+  getUserId() {
+    if (!this.isAuthenticated) {
+      throw new UnauthorizedHttpException('Unauthorized');
+    }
+    return this.decoded.id;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { AuthMiddleware } from './app/Services/AuthMiddleware';
+export { Authenticate } from './app/Services/Authenticate';
+export { Auth } from './app/Services/Facades/Auth';
