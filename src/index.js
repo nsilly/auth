e8a73c8a8331dd40b5739bddd481376208fee061
Hieupv <hieupv@codersvn.com>
2019-07-17 14:45:14 +0700

    add AuthOrGuestMiddleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { AuthMiddleware } from './app/Services/AuthMiddleware';
+export { AuthOrGuestMiddleware } from './app/Services/AuthOrGuestMiddleware';
 export { Authenticate } from './app/Services/Authenticate';
 export { Auth } from './app/Services/Facades/Auth';
